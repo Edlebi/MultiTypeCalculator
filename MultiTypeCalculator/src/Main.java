c933666635b001e99750edbcf8c511a1fea3114c
Edlebi <Hamzeh.ed@hotmail.com>
2024-07-29 16:50:32 +0300

    Moved to Main package

--- MultiTypeCalculator/src/Main.java
+++
@@ -1,8 +0,0 @@
-import UI.UserInterface;
-
-public class Main {
-    public static void main(String[] args) {
-        UserInterface userInterface = new UserInterface();
-        userInterface.choose();
-    }
-}
